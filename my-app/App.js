11e93501808586abe6c3b8b891ff1dcc6a2f1a9f
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-06-25 17:23:55 +1200

    input component added

--- my-app/App.js
+++ my-app/App.js
@@ -1,4 +1,5 @@
 import { View, StyleSheet } from "react-native";
+import { useState } from "react";
 
 import AppButton from "./app/components/AppButton";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -11,11 +12,30 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
     <GestureHandlerRootView>
-      <AccountScreen />
+      <Screen>
+        <AppPicker
+          selectedItem={category}
+          onSelectItem={(item) => setCategory(item)}
+          items={categories}
+          icon="apps"
+          placeholder="Category"
+        />
+        <AppTextInput icon="email" placeholder="Email" />
+      </Screen>
     </GestureHandlerRootView>
   );
 }
