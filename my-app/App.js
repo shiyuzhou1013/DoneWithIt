f51329f57ac300d2f4c310cc8990724441fda8bd
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-06-27 10:59:51 +1200

    listings edit screen image selector added

--- my-app/App.js
+++ my-app/App.js
@@ -13,26 +13,30 @@ import ImageInput from "./app/components/ImageInput";
 import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
+  // const [imageUris, setImageUris] = useState([]);
 
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
+  // const handleAdd = (uri) => {
+  //   setImageUris([...imageUris, uri]);
+  // };
 
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
+  // const handleRemove = (uri) => {
+  //   setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  // };
 
   return (
     // <GestureHandlerRootView>
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
-    </Screen>
+
+    // <Screen>
+    //   <ImageInputList
+    //     imageUris={imageUris}
+    //     onAddImage={handleAdd}
+    //     onRemoveImage={handleRemove}
+    //   />
+    // </Screen>
     // </GestureHandlerRootView>
+    <GestureHandlerRootView>
+      <ListingEditScreen />
+    </GestureHandlerRootView>
   );
 }
 
