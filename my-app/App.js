b4fcd68751da40a9cd2a47f0b97b96cdbd7587a7
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-06-26 19:18:34 +1200

    post image function added

--- my-app/App.js
+++ my-app/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet, Button } from "react-native";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -7,11 +7,39 @@ import ListItem from "./app/components/lists/ListItem";
 import Screen from "./app/components/Screen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import MessageScreen from "./app/screens/MessageScreen";
+import * as ImagePicker from "expo-image-picker";
+import { useEffect, useState } from "react";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("You need to ennable permission to access the library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.assets[0].uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
   return (
     <GestureHandlerRootView>
-      <ListingEditScreen />
+      <Screen>
+        <ImageInput
+          imageUri={imageUri}
+          onChangeImage={(uri) => setImageUri(uri)}
+        />
+      </Screen>
     </GestureHandlerRootView>
   );
 }
